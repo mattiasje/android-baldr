a25c99b3829666811bf15259f2308b997c64daa4
mattiasje <mattiasjerrewing@comhem.se>
2016-12-08 05:18:37 +0100

    Added stuff

--- app/src/main/java/com/gu/example/axel/baldr/AddLightActivity.java
+++ app/src/main/java/com/gu/example/axel/baldr/AddLightActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -25,6 +28,8 @@ public class AddLightActivity extends AppCompatActivity {
 
 
     private TextView textView;
+    private EditText addcode;
+    private Button add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +38,16 @@ public class AddLightActivity extends AppCompatActivity {
 
         Button btn = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.text);
+        addcode   = (EditText)findViewById(R.id.discoveryCode);
+        add = (Button) findViewById(R.id.button3);
 
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            addLight(view);
+
+            }
+        });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +62,25 @@ public class AddLightActivity extends AppCompatActivity {
     public void exitAdd(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+    public void addLight(View view) {
+        JSONObject jin =  new JSONObject();
+        JSONObject newLight = new JSONObject();
+        String code = addcode.getText().toString();
+
+        try {
+            // TODO: Get home id, this is app specific and read from SharedPreferences, if none exists on first run it needs to be generated.
+            // App will probably have this as a variable we can just use here.
+            String homeid="something";
+            jin.put("discoveryCode",code);
+            jin.put("home",homeid);
+            newLight.put("version", 1);
+            newLight.put("protocolName", "baldr");
+            newLight.put("discovery",jin);
+        }catch(JSONException e){
+            System.out.println(e);
+        }
+        // TODO send message using MQTTConnection
     }
 
 }
